92de9a83e07142e270406542f6fd52424225a94e
Henry <vinsen1109@gmail.com>
2020-11-13 17:43:56 +0800

    ver 1.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  rescue_from ActiveRecord::RecordNotFound,with: :error
+
+  private
+
+  def error
+   render file: 'public/404.html', 
+          layout: false ,
+          status: 404
+  end
+  
 end
--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,23 +1,54 @@
 class BoardsController < ApplicationController
 
+  before_action :find_board, only: [:show, :edit, :update, :destroy]
+
   def index
+    @boards = Board.all
+  end
+
+  def show
+
   end
   
   def new
-    @board = board.new
+    @board = Board.new
+  end
+
+  def edit
+
   end
 
   def create
-    clean_params = params.require(:board).permit(:title) #過濾資料指定欄位
-
-    @board = Board.new(clean_params) #產生實體init接資料指定給@board
+    @board = Board.new(board_params) #產生實體init接資料指定給@board
 
     if @board.save #如果資料正確就寫入資料
 
-    redirect_to "/" , notice '成功新增看板'
+    redirect_to "/" , notice: '成功新增看板'
 
     else
       render :new #render 'boards/new'
     end
   end
+
+  def update
+    if @board.update(board_params)
+    redirect_to boards_path, notice: '更新完成'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @board.destroy
+    redirect_to boards_path, notice: '看板已刪除'
+  end
+
+  private
+  def find_board
+    @board = Board.find(params[:id])
+end
+def board_params
+  clean_params = params.require(:board).permit(:title)
+end
 end
